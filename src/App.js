0a934d92407d763d9d6d8ac9ee939785adb131f5
vijay pratap <pratapvijayav4@gmail.com>
2022-08-31 12:45:59 +0530

    files are updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import NewsCard from './NewsCard'
+export default function App() {
 
-function App() {
+
+const [newsData,setNewsData] useState([]);
+const [loading,setLoading] = useState(true);
+
+let apiurl =https://newsapi.org/v2/top-headlines?country=in&apiKey=e70dffcac4be47f4b26b273e14766004
+
+useEffect(() =>{
+getApiDataViaFetch();
+},[])
+
+const getApiDataViaFetch =async()=>
+const apiResponece =await fetch(apiurl);
+const data = await apiResponece.json();
+console.log(data);
+setNewsData(data.articles);
+setLoading(false);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="absolute inset-0">
+        <div className="bg-white h-1/3 sm:h-2/3" />
+      </div>
+      <div className="relative max-w-7xl mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl">
+            Today's News
+          </h2>
+        </div>
+        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+         <NewsCard/>
+         <NewsCard/>
+         <NewsCard/>
+         <NewsCard/>
+         <NewsCard/>
+         </div>
+      </div>
     </div>
   );
 }
-
-export default App;
